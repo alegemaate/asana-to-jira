3032d039db864b75a01db7978d5e316f1dbf7e80
alegemaate <alegemaate@gmail.com>
2021-10-18 17:57:43 +0000

    Deploying to gh-pages from  @ 291a17f6b879291cdd2af23c1c1e106955809c08 🚀

---
+++ js/chunk-2d22d746.91d25f9e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23"),e={class:"about"},s=Object(c["i"])("h1",null,"This is an about page",-1),u=[s];function i(n,t){return Object(c["D"])(),Object(c["h"])("div",e,u)}var o=a("6b0d"),b=a.n(o);const d={},r=b()(d,[["render",i]]);t["default"]=r}}]);
+//# sourceMappingURL=chunk-2d22d746.91d25f9e.js.map
---
+++ precache-manifest.a93b618a354d5682ae242fed419d6078.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/asana-to-jira/asana-to-jira/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/asana-to-jira/asana-to-jira/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/asana-to-jira/asana-to-jira/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/asana-to-jira/asana-to-jira/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/asana-to-jira/asana-to-jira/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/asana-to-jira/asana-to-jira/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/asana-to-jira/asana-to-jira/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/asana-to-jira/asana-to-jira/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/asana-to-jira/asana-to-jira/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/asana-to-jira/asana-to-jira/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/asana-to-jira/asana-to-jira/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/asana-to-jira/asana-to-jira/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/asana-to-jira/asana-to-jira/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/asana-to-jira/asana-to-jira/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/asana-to-jira/asana-to-jira/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "1cfc6b3ee59030577510bff38fac8c5b",
+    "url": "/asana-to-jira/asana-to-jira/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/asana-to-jira/asana-to-jira/robots.txt"
+  },
+  {
+    "revision": "3c56388042a52f438d1e",
+    "url": "/asana-to-jira/css/app.1938134f.css"
+  },
+  {
+    "revision": "da01748cc65fe44d839f",
+    "url": "/asana-to-jira/css/chunk-vendors.d4799bbd.css"
+  },
+  {
+    "revision": "eb2792a8ee18fa1724dbcc61c0f19f42",
+    "url": "/asana-to-jira/index.html"
+  },
+  {
+    "revision": "3c56388042a52f438d1e",
+    "url": "/asana-to-jira/js/app.e0d77a2b.js"
+  },
+  {
+    "revision": "7305a25831e60f271c5d",
+    "url": "/asana-to-jira/js/chunk-2d22d746.91d25f9e.js"
+  },
+  {
+    "revision": "da01748cc65fe44d839f",
+    "url": "/asana-to-jira/js/chunk-vendors.196fc510.js"
+  },
+  {
+    "revision": "0c323fcca1d6fae2f2ffe87d7ebd28de",
+    "url": "/asana-to-jira/manifest.json"
+  }
+]);
